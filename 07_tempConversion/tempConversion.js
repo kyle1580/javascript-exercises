0699a51fb08b8ff6059c41baf5daf0726272ddf4
Kyle <kylecindric@gmail.com>
2022-01-29 13:28:03 -0600

    Finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  tempC = (5/9) * (tempF - 32);
+    if(tempC % Math.floor(tempC) != 0) {
+      return Number(tempC.toFixed(1));
+    } else {
+      return Number(tempC);
+    }
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  tempF = (tempC * (9/5)) + 32;
+  if(tempF % Math.floor(tempF) != 0) {
+    return Number(tempF.toFixed(1));
+  } else {
+    return Number(tempF);
+  }
 };
 
 // Do not edit below this line
