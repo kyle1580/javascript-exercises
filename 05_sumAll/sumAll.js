eceb1d855b5c9de8c00581db42f2c41e087ec81d
Kyle <kylecindric@gmail.com>
2022-01-29 12:36:38 -0600

    Finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    for(i=0; i<args.length; i++) {
+        if(typeof args[i] != "number") {
+            return "ERROR";
+        }
+    }
+    let start = Math.min(...args);
+    let stop = Math.max(...args);
+    if(start < 0 || stop < 0) {
+        return "ERROR";
+    }
+    let sum = start;
+    for(i=start+1; i<=stop; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
