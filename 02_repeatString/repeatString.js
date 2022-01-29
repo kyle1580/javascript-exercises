7eec440ca0a70fa67efae0c1cfb6f64a5cdb32a9
Kyle <kylecindric@gmail.com>
2022-01-29 11:11:35 -0600

    Finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string , repeats) {
+    if(repeats < 0) {
+        return "ERROR";
+    }
+    let repeated = [];
+    for(i=0; i<repeats; i++) {
+        repeated[i] = string;
+    }
+    return repeated.join("");
 };
 
 // Do not edit below this line
