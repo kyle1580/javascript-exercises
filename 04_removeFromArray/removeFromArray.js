6e3d34f35f27b1829c404a652dab9500f11cfd70
Kyle <kylecindric@gmail.com>
2022-01-29 12:15:49 -0600

    Finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    let array = arguments[0];
+    for(i=1; i<arguments.length; i++) {
+        if(array.indexOf(arguments[i]) < 0) {
+            // Do nothing
+        } else {
+            array.splice(array.indexOf(arguments[i]) , 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
