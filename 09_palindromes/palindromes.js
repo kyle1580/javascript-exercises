68382bc810f42f1a218e237636c05784890394d9
Kyle <kylecindric@gmail.com>
2022-02-02 13:05:45 -0600

    Finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
 const palindromes = function () {
-
+    let charArray = String(arguments[0]).toLocaleLowerCase().match(/\w/g);
+    let reversed = [...charArray].reverse();
+    return charArray.join('') == reversed.join('');
 };
 
 // Do not edit below this line
