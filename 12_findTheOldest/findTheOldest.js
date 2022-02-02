245f67c86de3f05f098b6b1d2b6cf2013a835c6b
Kyle <kylecindric@gmail.com>
2022-02-02 14:31:41 -0600

    Finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
 const findTheOldest = function() {
+    const people = arguments[0];
+    people.forEach(person => {
+        if("yearOfDeath" in person) {
 
+        } else {
+            person.yearOfDeath = 2022;
+        }
+    });
+    const oldest = people.sort(function(a , b) {
+        last = a.yearOfDeath - a.yearOfBirth;
+        next = b.yearOfDeath - b.yearOfBirth;
+        return last > next ? -1 : 1;
+    })
+    return oldest[0];
 };
 
 // Do not edit below this line
