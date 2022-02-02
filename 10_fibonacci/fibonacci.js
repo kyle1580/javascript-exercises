55a6ae08303c27bff82624e1b7e456e29498a163
Kyle <kylecindric@gmail.com>
2022-02-02 13:50:13 -0600

    Finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
 const fibonacci = function() {
-
+    if(arguments[0] < 0) {
+        return "OOPS";
+    }
+    let fibSeries = [1 , 1];
+    let i = 2;
+    while(fibSeries.length < arguments[0]) {
+        fibSeries[i] = fibSeries[i-1] + fibSeries[i-2];
+        i++;
+    }
+    return fibSeries[arguments[0] - 1];
 };
 
 // Do not edit below this line
